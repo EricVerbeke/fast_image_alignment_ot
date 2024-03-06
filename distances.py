5476f2a90a6dee391bac6014152a1e9f22362d01
EricVerbeke <eric.verbeke@utexas.edu>
2024-03-06 15:59:23 -0500

    updated code, added wemd and ot, mnist example

--- distances.py
+++ distances.py
@@ -1,4 +1,10 @@
+import utils
 import numpy as np
+
+import ftk  # PUT FTK HERE FOR NOW
+import pywt  # PUT WEMD HERE FOR NOW
+import ot  # PUT OT HERE FOR NOW
+
 
 
 def circulant_matvec(v, c):
@@ -85,9 +91,32 @@ def signed_pairwise_rotational_distances(images_pos, images_neg, n_points):
     return dists_dict
 
 
-def translate(array, ty, tx):
+def real_space_rotational_distance(image1, image2, angles):
+    ### Need to make this compatible with other distance functions
+    ### to be used in the alignment class as distance 'type'
+    ### rename this as brute force
+    
+    dists = []
+    
+    for a in angles:
+        image2_rot = utils.rotate(image2, -a)  # rotate clockwise as hack for now
+        dists.append(np.linalg.norm(image1 - image2_rot)**2)  # squared-norm to match fast conv
+        
+    return np.array(dists)
 
-    return np.roll(array, (ty, tx), axis=(0,1))
+
+def distance_matrix_from_dict(dists_dict, n_images):
+    
+    dist_mat = np.zeros((n_images, n_images))
+    
+    for key, dist in dists_dict.items():
+        
+        idx1, idx2 = key
+        dist_min = np.amin(dist)
+        dist_mat[idx1, idx2] = dist_min
+        dist_mat[idx2, idx1] = dist_min
+        
+    return dist_mat
 
 
 def fast_translation_distance(image1, image2):
@@ -108,6 +137,217 @@ def shift_align_images(image1, image2):
     
     ty, tx = fast_translation_distance(image1, image2)
     
-    image_2_align = translate(image2, -ty, -tx)
+    image_2_align = utils.translate(image2, -ty, -tx)
     
     return image_2_align
+
+
+
+
+
+##### PUTTING FTK / WEMD / POT HERE FOR NOW #####
+
+def ftk_precompute(reference, images, B):
+    
+    L = reference.shape[0]
+    n_images = images.shape[0]
+    
+    T = 2
+    N = L
+    ftk_eps = 1e-2
+    
+    delta_range = 1*B
+    oversampling = 1
+    
+    rmax = N / 2*np.pi
+    ngridr = 3*N
+    n_psi = 360
+    
+    # the following four lines of code perform the necessary precompution for the fourier grids
+    pf_grid = ftk.make_tensor_grid(rmax, ngridr, n_psi) # grid for polar fourier transform
+    tr_grid = ftk.make_adaptive_grid(delta_range, T/N, oversampling) # grid for shift
+    n_bessel = int(np.ceil(delta_range))
+    plan = ftk.ftk_plan(tr_grid, pf_grid, n_bessel, ftk_eps)
+    
+    # the following code computes the polar fourier transforms of the images 
+    Shat = ftk.cartesian_to_pft(images, T, pf_grid)
+    Mhat = ftk.cartesian_to_pft(reference.reshape((1,L,L)), T, pf_grid)
+    
+    prods_ftk, tm_ftk = ftk.ftk_execute(plan, Mhat, Shat)
+    
+    ftk_parameters = [n_images, prods_ftk, pf_grid, n_psi, tr_grid]
+    
+    return ftk_parameters
+    
+
+def ftk_align(reference, images, B):
+    
+    ftk_parameters = ftk_precompute(reference, images, B)
+    n_images = ftk_parameters[0]
+    prods_ftk = ftk_parameters[1]
+    pf_grid = ftk_parameters[2]
+    n_psi = ftk_parameters[3]
+    tr_grid = ftk_parameters[4]
+    
+    corr=np.zeros(n_images)  # the maximum correlation found (theoretical maximum should be 1)
+    angle_rec=np.zeros(n_images)  # the in-plane rotation angle recovered
+    shift_rec=np.zeros((n_images,2))  # the shift recovered
+    
+    for idx in range(n_images):      
+        index=np.unravel_index(prods_ftk[0,idx,:,:].argmax(), prods_ftk[0,idx,:,:].shape)
+        corr[idx]=prods_ftk[0,idx,:,:].max()
+        angle_rec[idx]=pf_grid['all_psi'][:n_psi][index[0]]*180/np.pi 
+        shift_rec[idx]=tr_grid['trans'][index[1]]
+    
+    return corr, angle_rec, shift_rec
+
+
+def embed(arr, wavelet, level):
+    """
+    *** This code is borrowed from: https://github.com/RuiyiYang/BOTalign/blob/main/wemd.py#L11
+    This function computes an embedding of Numpy arrays such that the L1 distance
+    between the resulting embeddings is approximately equal to the Earthmover distance of the arrays.
+
+    Input:
+        arr - numpy array.
+        level - Decomposition level of the wavelets. Larger levels yield more coefficients and more accurate results.
+        wavelet - either the name of a wavelet supported by PyWavelets (e.g. 'coif3', 'sym3') or a pywt.Wavelet object.
+                  See https://pywavelets.readthedocs.io/en/latest/ref/wavelets.html#built-in-wavelets-wavelist
+    Output:
+        One-dimensional numpy array containing weighted details coefficients.
+        Approximate Earthmover's distances are then given by the l_1 distances of the results, e.g.
+            wemd(arr1, arr2) := numpy.linalg.norm(embed(arr1, wavelet, level)-embed(arr2, wavelet, level), ord=1)
+    """
+
+    arrdwt = pywt.wavedecn(arr/arr.sum(), wavelet, mode='zero', level=level)
+
+    dimension = len(arr.shape)
+    assert dimension in [2,3]
+
+    n_levels = len(arrdwt[1:])
+
+    weighted_coefs = [arrdwt[0].flatten()*2**(n_levels)]
+    for (j, details_level_j) in enumerate(arrdwt[1:]):
+        for coefs in details_level_j.values():
+            multiplier = 2**((n_levels-1-j)*(1+(dimension/2.0)))
+            weighted_coefs.append(coefs.flatten()*multiplier)
+
+    return np.concatenate(weighted_coefs)
+
+
+def wemd_rotational_distance(image1, image2, angles, wavelet='coif3', level=1):
+    
+    ### *** How to set the wavelet and level???
+    
+    dists = []
+    
+    w1 = embed(image1, wavelet, level)
+    
+    for a in angles:
+        image2_rot = utils.rotate(image2, -a)
+        w2 = embed(image2_rot, wavelet, level)
+        dists.append(np.linalg.norm(w1 - w2, ord=1))
+        
+    return np.array(dists)
+
+
+def rotational_wasserstein_distance(image1, image2, angles, M):
+        
+    dists = []
+    image1_flat = image1.flatten()
+
+    for a in angles:
+        image2_rot = utils.rotate(image2, -a)
+        image2_rot = np.where(image2_rot<0, 0, image2_rot)
+        image2_rot = image2_rot / np.sum(image2_rot)
+        image2_flat = image2_rot.flatten()
+
+        dists.append(ot.emd2(image1_flat, image2_flat, M))
+
+    return np.array(dists)    
+
+
+def compute_transport_matrix(image):
+    
+    ny, nx = image.shape
+    xs = np.arange(nx)
+    xgrid, ygrid = np.meshgrid(xs, xs)
+    points = np.array(list(zip(ygrid.flatten(), xgrid.flatten())))  # order matches flatten convention
+    M = ot.dist(points, points)  # returns the 2-norm squared, change metric='euclidean' for w1    
+    
+    return M
+
+
+
+# ### separate the precompute part of FTK code to speed up
+# def ftk_align(templates, img, B):
+#     '''
+#     inputs:
+#         templates: an ntemp x L x L matrix, where ntemp = the number of images to be aligned and L is the size of the images
+#         img: an L x L matrix, representing a single image
+#         B: a number, representing the window size for shift. The shifts to be searched over lie in [-B,B]^2.   
+    
+    
+#     returns: (see more at the end of this function)
+#         corr: maximum correlation found
+#         angle_rec: optimal in-plane rotation angle
+#         shift_rec: optimal shift
+#     '''
+
+  
+    
+#     L=img.shape[0]; ntemp=templates.shape[0]
+    
+#     T=2; N=L; ftk_eps=1e-2; 
+#     # These parameters are used by the authors and I didn't play much around with their choices  
+    
+#     delta_range=1*B; oversampling=1 
+#     # delta_range = maximum size of shifts in each dimension 
+#     # oversampling can only be an integer, which controls the mesh size of discretization
+#     # oversampling does not change the window size
+#     # oversampling=1 is good enough and oversampling=2 gives a finer discretization
+    
+#     rmax=N/2*np.pi; ngridr=3*N; n_psi=360 
+#     # ngridr = the number of grids in the radial direction; n_psi = the number of grids in theta direction
+#     # my experience is that ngridr=N; n_psi=90 is good enough  
+    
+    
+#     # the following four lines of code perform the necessary precompution for the fourier grids
+#     pf_grid = ftk.make_tensor_grid(rmax, ngridr, n_psi) # grid for polar fourier transform
+#     tr_grid = ftk.make_adaptive_grid(delta_range, T/N, oversampling) # grid for shift
+#     n_bessel = int(np.ceil(delta_range))
+#     plan = ftk.ftk_plan(tr_grid, pf_grid, n_bessel, ftk_eps)
+    
+    
+#     # the following code computes the polar fourier transforms of the images 
+#     Shat = ftk.cartesian_to_pft(templates, T, pf_grid)
+#     Mhat = ftk.cartesian_to_pft(img.reshape((1,L,L)), T, pf_grid)
+    
+#     # everything up to here can be treated as precomputation and can be separated from the rest 
+#     # of the code
+    
+    
+
+#     # the following code computes all the inner products between 
+#     # templates and shifted&rotated versions of img 
+#     # (or shifted&rotated versions of templates and img)
+#     # prods_ftk is a 1 x ntemp x n_psi x n_trans matrix, where n_trans is the number of translation grid
+#     prods_ftk, tm_ftk = ftk.ftk_execute(plan, Mhat, Shat)
+    
+    
+#     # the following code finds for each image in templates the optimal rotation and shift.
+#     # this is done by finding first the index of largest inner product for each image
+#     # and then recover the rotation and shift from pf_grid and tr_grid
+#     # PS: the shift recovery is probably wrong but I never figured out the correct way
+
+#     corr=np.zeros(ntemp); angle_rec=np.zeros(ntemp); shift_rec=np.zeros((ntemp,2))
+#     # corr = the maximum correlation found (theoretical maximum should be 1)
+#     # angle_rec = the in-plane rotation angle recovered
+#     # shift_rec = the shift recovered
+#     for i in range(ntemp):      
+#         index=np.unravel_index(prods_ftk[0,i,:,:].argmax(), prods_ftk[0,i,:,:].shape)
+#         corr[i]=prods_ftk[0,i,:,:].max()
+#         angle_rec[i]=pf_grid['all_psi'][:n_psi][index[0]]*180/np.pi; 
+#         shift_rec[i]=tr_grid['trans'][index[1]]
+    
+#     return corr, angle_rec, shift_rec
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import gzip
 import numpy as np
 from scipy import ndimage as ndi
 
@@ -32,6 +33,38 @@ def sphere_mask(rdists, radius=False):
 def rotate(array, degrees, reshape=False):
     
     return ndi.rotate(array, degrees, reshape=reshape)
+
+
+def translate(array, ty, tx):
+
+    return np.roll(array, (ty, tx), axis=(0,1))
+
+
+def zero_pad_image_stack(image_stack, w):
+    
+    return np.pad(image_stack, pad_width=((0, 0), (w, w), (w, w)))
+
+
+def load_mnist_images(image_file):
+    with gzip.open(image_file, 'r') as f:
+        magic_number = int.from_bytes(f.read(4), 'big') # first 4 bytes is a magic number
+        image_count = int.from_bytes(f.read(4), 'big')  # second 4 bytes is the number of images
+        row_count = int.from_bytes(f.read(4), 'big') # third 4 bytes is the row count
+        column_count = int.from_bytes(f.read(4), 'big') # fourth 4 bytes is the column count
+        image_data = f.read() # rest is the image pixel data stored as an unsigned byte, values are 0 to 255        
+        images = np.frombuffer(image_data, dtype=np.uint8).reshape((image_count, row_count, column_count))
+        
+        return images
+
+    
+def load_mnist_labels(label_file):
+    with gzip.open(label_file, 'r') as f:
+        magic_number = int.from_bytes(f.read(4), 'big') # first 4 bytes is a magic number
+        label_count = int.from_bytes(f.read(4), 'big') # second 4 bytes is the number of labels
+        label_data = f.read() # rest is the label data stored as unsigned byte, values are 0 to 9
+        labels = np.frombuffer(label_data, dtype=np.uint8)
+        
+        return labels
 
 
 ##### add/test these functions later
